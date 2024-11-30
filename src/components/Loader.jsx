0aa505395478f2d8c6837beb6ae125f60a6b5f4f
AlicjaSzamraj <120415960+AlicjaSzamraj@users.noreply.github.com>
2024-11-30 11:08:04 +0000

    creating a loader from react library

--- src/components/Loader.jsx
+++ src/components/Loader.jsx
@@ -1,10 +1,20 @@
 import React from "react";
+import { TailSpin } from "react-loader-spinner";
 import styles from "../styles/Loader.module.css";
 
 const Loader = () => {
   return (
     <div className={styles.Loader}>
-      <div className="spinner"></div>
+      <TailSpin
+        height="80"
+        width="80"
+        color="#4fa94d"
+        ariaLabel="tail-spin-loading"
+        radius="1"
+        wrapperStyle={{}}
+        wrapperClass=""
+        visible={true}
+      />
     </div>
   );
 };
